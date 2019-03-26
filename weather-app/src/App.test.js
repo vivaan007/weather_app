9ccc29cbbe36a95ae9e340baa44dfb6ab73c7d33
Tripathi <vtripathi1@MTVL167a0e261.local>
2019-03-26 01:02:20 -0700

    test conf and few tests added

--- weather-app/src/App.test.js
+++ weather-app/src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import { shallow } from 'enzyme';
 import { resolve } from 'path';
 var chakram = require('chakram');
-var expect = chakram.expect;
+var expectC = chakram.expect;
 import Config from "./config.js"
 
 it('renders without crashing', () => {
@@ -12,10 +15,21 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
+it('renders without crashing 2', () => {
+  shallow(<App />);
+});
+
+it('renders input Form and checks for Submit button', () => {
+  const form = shallow(<Form />);
+  const button = <button>Get Weather</button>;
+  //expect(wrapper.contains(form)).toEqual(true);
+  expect(form).toContainReact(button);
+});
+
 it('tests that api gives response for the correct city ', async () => {
   var inCity = "Sunnyvale"
   var res = await chakram.get("http://api.openweathermap.org/data/2.5/weather?q=" + inCity + "&appid=" + Config.API_KEY);
   var outCity = res.response.body.name
-  return ((expect(res).to.have.status(200)) && (expect(outCity).to.equal("Sunnyvale")));
+  return ((expectC(res).to.have.status(200)) && (expectC(outCity).to.equal("Sunnyvale")));
 
 });
